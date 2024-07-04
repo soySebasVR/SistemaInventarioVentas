f5cd96cf7c3048018da1b97aa1fe141a4c92a223
Sebastian Vasquez Davila <sebastian.vasquez@fractalservicios.pe>
2024-07-04 18:12:48 -0500

    cambios producto

--- src/Producto.java
+++ src/Producto.java
@@ -9,58 +9,65 @@ public class Producto {
     int cantidad;
     double precio;
 
-    public static Producto getByNombre(String search) throws FileNotFoundException {
+    public static Producto buscar(String buscar) {
         Producto prod = new Producto();
-        prod.nombre = search;
+        prod.nombre = buscar;
         prod.cantidad = 0;
         prod.precio = 0;
 
         File bdFile = new File("bd.csv");
-        Scanner myReader = new Scanner(bdFile);
-
-        while (myReader.hasNextLine()) {
-            String data = myReader.nextLine();
-            String regex = "[,]";
-            String[] myArray = data.split(regex);
-            if (myArray[0].equals(search)) {
-                prod.nombre = myArray[0];
-                prod.cantidad = Integer.valueOf(myArray[1]);
-                prod.precio = Double.valueOf(myArray[2]);
+        try (Scanner myReader = new Scanner(bdFile)) {
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String regex = "[,]";
+                String[] myArray = data.split(regex);
+                if (myArray[0].equals(buscar)) {
+                    prod.nombre = myArray[0];
+                    prod.cantidad = Integer.valueOf(myArray[1]);
+                    prod.precio = Double.valueOf(myArray[2]);
+                }
             }
+            myReader.close();
+        } catch (NumberFormatException | FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-        myReader.close();
 
         return prod;
     }
 
-    public void save() throws FileNotFoundException {
+    public void guardar() {
         boolean escribio = false;
         File bdFile = new File("bd.csv");
-        Scanner myReader = new Scanner(bdFile);
-        String newBd = "";
+        try (Scanner myReader = new Scanner(bdFile)) {
+            String newBd = "";
 
-        while (myReader.hasNextLine()) {
-            String data = myReader.nextLine();
-            String regex = "[,]";
-            String[] myArray = data.split(regex);
-            if (myArray[0].equals(this.nombre)) {
-                newBd += this.nombre + "," + this.cantidad + "," + this.precio;
-                escribio = true;
-            } else {
-                newBd += data;
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String regex = "[,]";
+                String[] myArray = data.split(regex);
+                if (myArray[0].equals(this.nombre)) {
+                    newBd += this.nombre + "," + this.cantidad + "," + this.precio;
+                    escribio = true;
+                } else {
+                    newBd += data;
+                }
+                if (myReader.hasNextLine()) {
+                    newBd += "\n";
+                } else if (!escribio) {
+                    newBd += "\n" + this.nombre + "," + this.cantidad + "," + this.precio;
+                }
             }
-            if (myReader.hasNextLine()) {
-                newBd += "\n";
-            } else if (!escribio) {
-                newBd += "\n" + this.nombre + "," + this.cantidad + "," + this.precio;
-            }
-        }
-        myReader.close();
+            myReader.close();
 
-        bdFile.delete();
-        try (FileWriter myWriter = new FileWriter("bd.csv")) {
-            myWriter.write(newBd);
-        } catch (IOException e) {
+            bdFile.delete();
+            try (FileWriter myWriter = new FileWriter("bd.csv")) {
+                myWriter.write(newBd);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
